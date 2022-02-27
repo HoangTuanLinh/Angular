505254c4b30cab37448b44c92316d1c229ab5134
HoangTuanLinh <mrtuanlinh1999@gmail.com>
2022-02-27 16:10:39 +0700

    assignment1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from "./login/login.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
+import {RegisterComponent} from "./Register/register.component";
+import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {RouterModule, Routes} from "@angular/router";
+import {CurrentWeatherComponent} from "./currentweather/currentweather.component";
+import {FormsModule} from "@angular/forms";
+import {ForecastComponent} from "./forecast/forecast.component";
 
+const appRoutes:Routes = [
+  {path:"",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"forgot-password",component:ForgotPasswordComponent},
+  {path:"current-weather",component:CurrentWeatherComponent},
+  {path:"forecast",component:ForecastComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,FooterComponent,HeaderComponent,LoginComponent,RegisterComponent,ForgotPasswordComponent,CurrentWeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "forgot-password",
+  templateUrl: './forgotpassword.component.html',
+})
+export class ForgotPasswordComponent {
+}
