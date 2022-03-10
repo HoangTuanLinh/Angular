8c6b96b1387e14f6879266072a83a5042b9e19bb
HoangTuanLinh <mrtuanlinh1999@gmail.com>
2022-03-10 21:56:49 +0700

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from "./login/login.component";
-import {FooterComponent} from "./footer/footer.component";
-import {HeaderComponent} from "./header/header.component";
-import {RegisterComponent} from "./Register/register.component";
+import {SigninComponent} from "./signin/signin.component";
+import {RegisterComponent} from "./register/register.component";
 import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 import {RouterModule, Routes} from "@angular/router";
-import {CurrentWeatherComponent} from "./currentweather/currentweather.component";
 import {FormsModule} from "@angular/forms";
+import {CurrentWeatherComponent} from "./currentweather/currentweather.component";
+import {HttpClientModule} from "@angular/common/http";
 import {ForecastComponent} from "./forecast/forecast.component";
 
-const appRoutes:Routes = [
-  {path:"",component:LoginComponent},
-  {path:"register",component:RegisterComponent},
-  {path:"forgot-password",component:ForgotPasswordComponent},
-  {path:"current-weather",component:CurrentWeatherComponent},
-  {path:"forecast",component:ForecastComponent}
-]
+const appRoutes: Routes =[
+  {path:"", component:SigninComponent},
+  {path:"forgotpassword", component:ForgotPasswordComponent},
+  {path:"regiter", component:RegisterComponent},
+  {path:"currentweather", component:CurrentWeatherComponent},
+  {path:"forecast", component:ForecastComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent,FooterComponent,HeaderComponent,LoginComponent,RegisterComponent,ForgotPasswordComponent,CurrentWeatherComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    HeaderComponent,
+    FooterComponent,
+    CurrentWeatherComponent,ForecastComponent,
+
   ],
   imports: [
-    BrowserModule,FormsModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -4,29 +4,31 @@ import {Component} from "@angular/core";
   templateUrl:"./forecast.component.html"
 })
 export class ForecastComponent{
-  weathers = [
-    {
-      city:"Hanoi, Vietnam",
-      temp: 15,
-      hump: 95,
-      press: 1005,
-      desc:"Trời rét, không sương mù, không mây",
-      icon:"04n"
-    },
-    {
-      city:"Hanoi, Vietnam",
-      temp: 15,
-      hump: 95,
-      press: 1005,
-      desc:"Trời rét, không sương mù, không mây",
-      icon:"04n"
-    }, {
-      city:"Hanoi, Vietnam",
-      temp: 15,
-      hump: 95,
-      press: 1005,
-      desc:"Trời rét, không sương mù, không mây",
-      icon:"04n"
-    }
-  ];
+  weathers:any = [];
+
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(){
+    const url = "https://api.openweathermap.org/data/2.5/forecast";
+    let parameters = new HttpParams();
+    parameters = parameters.append("q","Hanoi,vietnam");
+    parameters = parameters.append("appid","09a71427c59d38d6a34f89b47d75975c");
+    parameters = parameters.append("units","metric");
+    this.http.get(url,{
+      params:parameters
+    }).subscribe((rs:any)=>{
+      let list = rs.list;
+      for(var i=0;i<list.length;i++){
+        var w = {
+          time:  list[i].dt_txt,
+          temp:  list[i].main.temp,
+          hump: list[i].main.humidity,
+          press: list[i].main.pressure,
+          desc:  list[i].weather[0].description,
+          icon:  list[i].weather[0].icon,
+        }
+        this.weathers.push(w);
+      }
+    })
+  }
 }
